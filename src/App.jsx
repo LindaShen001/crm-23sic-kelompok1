f4adf1b622099245c86a6070c7fd679c82faaec8
Linda <yshen@gmail.com>
2025-06-04 12:30:57 +0700

    Ini main terbaru

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Dashboard from "./pages/Dashboard";
-import SalesManagement from "./pages/SalesManagement";
 import Pelanggan from "./pages/Pelanggan";
 import HealthCampaign from "./pages/HealthCampaign";
 
